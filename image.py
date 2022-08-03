05540237e98fbad852931965b7381070c893e7bc
Luca Giantomasi <luca.giantomasi@toconsulting.it>
2022-08-03 16:11:06 +0200

    Added change screen with pinch gesture

--- image.py
+++ image.py
@@ -8,12 +8,33 @@ from kivy.clock import Clock
 import cv2
 import mediapipe as mp
 import json
+import tensorflow as tf
+from collections import Counter
+from collections import deque
+import utils
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5
 )
+pose_model = tf.keras.models.load_model("keypoint_classifier.hdf5")
+gesture_model = tf.keras.models.load_model("point_history_classifier.hdf5")
+
+poses_class_names = ["Open", "Fist", "Point"]
+gesture_class_names = [
+    "Stop",
+    "Clockwise",
+    "Counter Clockwise",
+    "Move",
+    "Pinch",
+    "Unpinch"
+]
+history_length = 16
+point_history = deque(maxlen=history_length)
+
+finger_gesture_history = deque(maxlen=history_length)
 
 
 class MovingCircle(Widget):
@@ -31,11 +52,7 @@ class ImageWidget(Scatter):
             data = json.load(f)
             self.source = "images/" + data['source']
 
-    def on_touch_down(self, touch):
-        app = App.get_running_app()
-
-        app.root.current = "second"
-        app.root.transition.direction = "left"
+    # def on_touch_down(self, touch):
     #     # # Override Scatter's `on_touch_down` behavior for mouse scroll
     #     if touch.button == 'left':
     #         factor = 1.1
@@ -66,11 +83,123 @@ class MainWindow(Screen):
                           hand_landmarks.landmark[9].y * self.height]
                 self.user_hand.center = center
 
+                landmark_list = utils.calc_landmark_list(
+                    img, hand_landmarks)
+
+                # POSE RECOGNITION
+                pre_processed_landmark_list = utils.pre_process_landmark(
+                    landmark_list)
+
+                tmp = pose_model.predict([pre_processed_landmark_list])
+                result = np.amax(np.squeeze(tmp))
+                pose_class_name = poses_class_names[np.argmax(
+                    np.squeeze(tmp))]
+
+                if result < 0.7:
+                    pose_class_name = "Other"
+
+                # GESTURE RECOGNITION
+                pre_processed_point_history_list = utils.pre_process_point_history(
+                    img, point_history
+                )
+
+                if pose_class_name == "Point":
+                    point_history.append(landmark_list[8])
+                else:
+                    point_history.append([0, 0])
+
+                finger_gesture_id = 0
+                point_history_len = len(pre_processed_point_history_list)
+                if point_history_len == (history_length * 2):
+                    finger_gesture_id = np.argmax(
+                        np.squeeze(
+                            gesture_model.predict(
+                                np.array(
+                                    [pre_processed_point_history_list], dtype=np.float32
+                                )
+                            )
+                        )
+                    )
+
+                finger_gesture_history.append(finger_gesture_id)
+                most_common_fg_id = Counter(
+                    finger_gesture_history).most_common()
+                gesture_class_name = gesture_class_names[most_common_fg_id[0][0]]
+                if gesture_class_name == "Pinch":
+                    app = App.get_running_app()
+                    app.root.current = "second"
+                    app.root.transition.direction = "left"
+                    point_history.clear()
+                    finger_gesture_history.clear()
+
 
 class SecondWindow(Screen):
 
+    def __init__(self, **kwargs):
+        super(SecondWindow, self).__init__(**kwargs)
+        # TODO dynamically set image source based on hand position
+        self.source = "images/wallpaper.jpg"
+
     def update(self):
-        return True
+        _, img = cam.read()
+        img = cv2.flip(img, -1)
+        img.flags.writeable = False
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img)
+
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                landmark_list = utils.calc_landmark_list(
+                    img, hand_landmarks)
+
+                # POSE RECOGNITION
+                pre_processed_landmark_list = utils.pre_process_landmark(
+                    landmark_list)
+
+                tmp = pose_model.predict([pre_processed_landmark_list])
+                result = np.amax(np.squeeze(tmp))
+                pose_class_name = poses_class_names[np.argmax(
+                    np.squeeze(tmp))]
+
+                if result < 0.7:
+                    pose_class_name = "Other"
+
+                # GESTURE RECOGNITION
+                pre_processed_point_history_list = utils.pre_process_point_history(
+                    img, point_history
+                )
+
+                if pose_class_name == "Point":
+                    point_history.append(landmark_list[8])
+                else:
+                    point_history.append([0, 0])
+
+                finger_gesture_id = 0
+                point_history_len = len(pre_processed_point_history_list)
+                if point_history_len == (history_length * 2):
+                    finger_gesture_id = np.argmax(
+                        np.squeeze(
+                            gesture_model.predict(
+                                np.array(
+                                    [pre_processed_point_history_list], dtype=np.float32
+                                )
+                            )
+                        )
+                    )
+
+                finger_gesture_history.append(finger_gesture_id)
+                most_common_fg_id = Counter(
+                    finger_gesture_history).most_common()
+                gesture_class_name = gesture_class_names[most_common_fg_id[0][0]]
+                if gesture_class_name == "Unpinch":
+                    app = App.get_running_app()
+                    app.root.current = "main"
+                    app.root.transition.direction = "right"
+                    point_history.clear()
+                    finger_gesture_history.clear()
 
 
 class WindowManager(ScreenManager):
